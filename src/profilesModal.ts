afe24108532e13b9999afc16b4133fc58978f1bc
caasion <isaacngcaasi@gmail.com>
2022-09-18 20:26:12 -0400

    added click event

--- src/profilesModal.ts
+++ src/profilesModal.ts
@@ -4,27 +4,32 @@ import type TypingTransformer from "./main";
 export class ProfileModal extends Modal {
   plugin: TypingTransformer;
   profiles: string[];
-  selectedProfileName: string;
+  selectedProfile: string;
   
   constructor(app: App, plugin: TypingTransformer) {
     super(app);
     this.plugin = plugin;
     this.profiles = plugin.settings.profiles.map(p => p.title);
-    this.selectedProfileName = plugin.settings.activeProfile;
+    this.selectedProfile = plugin.settings.activeProfile;
   }
   
   //TODO: Create function to handle selected profile
+
 
   onOpen() {
     const {contentEl} = this;
     const wrapperEl = contentEl.createEl('div');
     this.profiles.forEach(p => {
       const profile = wrapperEl.createEl('p', {text: p});
-      profile.addEventListener('click', () => this.selectedProfileName = p)
+      profile.addEventListener('click', () => {
+        this.selectedProfile = p;
+        this.close();
+      })
     });
   }
 
   onClose() {
+    console.log(this.selectedProfile)
     const {contentEl} = this;
     contentEl.empty();
   }
